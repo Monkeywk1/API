949e941b94fc8b3f35d059ca63b68a17b4f16e27
David Alzate <david.alzate777@gmail.com>
2024-08-13 15:53:28 -0500

    Inicializacion de API

---
+++ router/mascotas.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+/*crear mascota */
+router.post('/mascotas', (req, res) => {
+    res.send('creacion de mascota')
+})
+module.exports = router;
